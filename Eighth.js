a541a51d8e3ac3bd016eea7b9d641820f5852bbc
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-07-18 10:30:22 +0530

    Eighth Program Palindrome

---
+++ Eighth.js
@@ -0,0 +1,9 @@
+let a = "121";
+let b = a[0];
+let c = a.length-1
+if(b.toUpperCase()==a[c].toUpperCase()){
+    console.log("Yes This is Palindrome :-"+a);
+}
+else{
+    console.log("No This is Not Palindrome :-"+a);
+}
