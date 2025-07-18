5e0c6b5c89ae2e8a1b1416e7dd272d91ccf45211
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-07-18 11:32:27 +0530

    Eleventh Program not complelted

---
+++ Elevnth.js
@@ -0,0 +1,8 @@
+let a= 123;
+let sum =0;
+
+
+for(let i = 0;i<a.length-1;i++){
+    sum +=a[i];
+}
+console.log(sum)
---
+++ Tenth.js
@@ -0,0 +1,6 @@
+let num ='5045';
+let count=0;
+for(let i =1 ; i<=num.length;i++){
+    count++;
+}
+console.log(count);
