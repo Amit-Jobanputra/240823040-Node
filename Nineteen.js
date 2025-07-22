71e1deb9d13582c3036832fa5a0a1434d45e9eb4
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-07-22 13:24:02 +0530

    Exponatnt Finding

---
+++ Nineteen.js
@@ -0,0 +1,10 @@
+let a =20;
+if(a>0){
+    console.log(a+' is a Positive Number');
+}
+else if(a<0){
+    console.log(a+'is a Negative Number');
+}
+else{
+    console.log(a+'is a Zero');
+}
---
+++ Twenty.js
@@ -0,0 +1,7 @@
+let a = 2
+let b = 3
+let num = 1;
+for (let i = 0; i < power; ++i) {
+    num = num * n;
+}
+console.log(num);
